c7e635f18c68d569869cc18bb081626ffda24848
Emptyst1122 <155888605+Emptyst1122@users.noreply.github.com>
2024-01-07 15:34:09 +0300

    Add files via upload

---
+++ js/code.js
@@ -0,0 +1,17 @@
+﻿function generate(length, charset) {
+   let password = '';
+   for(let i = 0; i < length; ++i){
+      password += charset[
+          Math.floor(
+              Math.random() * charset.length
+         )
+     ];
+   }
+   return password;
+}
+jQuery('button').on('click', function(){
+var lab, ad
+ad=jQuery('input').val();
+lab=generate(ad, '+-*!&?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890');
+jQuery('body').append(lab,'<br>');
+});
